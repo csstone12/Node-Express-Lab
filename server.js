1caef20fd50e1ecf8b558a7b5f4b11c32dbe9e29
csstone12 <36163530+csstone12@users.noreply.github.com>
2018-04-03 18:25:49 -0400

    Stone Chris | Node Lab
    Stone Chris | Node Lab

--- server.js
+++ server.js
@@ -1,11 +1,106 @@
 const express = require('express');
 const server = express();
 const db = require('./data/db.js');
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const port = 5000;
-server.listen(port, () => console.log('API Running on port 5000'));
 
-server.get('/', function(req, res){
-    res.send({ api: 'Running....'})
+server.listen(port, () => console.log('API Running on port 5000'));
+server.use(cors());
+server.use(bodyParser.json());
+
+server.get('/api/posts', function(req, res){
+    db
+        .find()
+        .then(posts => {
+            res.json(posts);
+        })
+    
+        .catch(error => {
+            res.status(500).json({
+                errorMessage: "The posts' information could not be retrieved."
+        });
+    });
 });
 
-// add your server code starting here
+server.get("/api/posts/:id", (req, res) => {
+  db
+    .findById(req.params.id)
+    .then(posts => {
+      if (posts.length < 1) {
+        res.status(404).json({ error: "That post doesn't exist." });
+      } else {
+        res.status(200).json(posts);
+      }
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ message: "The post information could not be retrieved." });
+    });
+});
+
+server.post("/api/posts/", (req, res) => {
+  console.log(req);
+  if (!req.body)
+    return res
+      .status(400)
+      .json({ errorMessage: "Please provide title and contents for post" });
+  db
+    .insert(req.body)
+    .then(post => {
+      res.status(201).json(req.body);
+    })
+    .catch(error => {
+      res.status(500).json({
+        errorMessage:
+          "There was an error while saving the post to the database."
+      });
+    });
+});
+
+server.delete("/api/posts/:id", (req, res) => {
+  db
+    .remove(req.params.id)
+    .then(posts => {
+      if (posts < 1) {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      } else {
+        res.json(posts);
+      }
+    })
+    .catch(error => {
+      res.status(500).json({
+        errorMessage: "The post could not be removed."
+      });
+    });
+});
+
+server.put("/api/posts/:id", (req, res) => {
+  if (!req.body) {
+    res.status(400).json({
+      errorMessage: "Please provide title and contents for the post."
+    });
+  }
+  db
+    .update(req.params.id, req.body)
+    .then(posts => {
+      if (posts > 0) {
+        res.status(200).json(req.body);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      }
+    })
+    .catch(() =>
+      res
+        .status(500)
+        .json({ error: "The post information could not be modified." })
+    );
+});
+server.listen(port, () => {
+  console.log("API Running");
+});
