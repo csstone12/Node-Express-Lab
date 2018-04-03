aa7f2fafb919d4c030105d7030b870214689f83a
csstone12 <36163530+csstone12@users.noreply.github.com>
2018-04-02 20:46:26 -0400

    Stone Chris | Node Express Lab
    Stone Chris | Node Express Lab

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
-// import your node modules
-
+const express = require('express');
+const server = express();
 const db = require('./data/db.js');
+const port = 5000;
+server.listen(port, () => console.log('API Running on port 5000'));
+
+server.get('/', function(req, res){
+    res.send({ api: 'Running....'})
+});
 
 // add your server code starting here
